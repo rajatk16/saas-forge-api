dad9159433686a0c24691829c41f90e0c39023a1
sudagar <rajatsudagade@autodesk.com>
2025-07-12 12:19:59 +0530

    addressed review comments

--- src/user/guards/JwtAuth.guard.ts
+++ src/user/guards/JwtAuth.guard.ts
@@ -3,15 +3,17 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  /**
+   * @param err - The error object
+   * @param user - The user object
+   * @param info - The info object
+   * @returns The user object
+   */
   handleRequest(err: any, user: any, info: any) {
     if (err || !user) {
-      if (info?.name === 'TokenExpiredError') {
-        throw new UnauthorizedException('Token expired');
-      }
-      if (info?.message) {
-        throw new UnauthorizedException(info.message);
-      }
-      throw new UnauthorizedException('Invalid token');
+      const exceptionMessage =
+        info?.name === 'TokenExpiredError' ? 'Token expired' : info?.message ? info.message : 'Invalid token';
+      throw new UnauthorizedException(exceptionMessage);
     }
     return user;
   }
