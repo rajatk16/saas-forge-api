be32c7416dd06b4ec815f3508b7d33c09c829c27
sudagar <rajatsudagade@autodesk.com>
2025-07-12 12:14:50 +0530

    custom error message for token expiration

--- src/user/guards/JwtAuth.guard.ts
+++ src/user/guards/JwtAuth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      if (info?.name === 'TokenExpiredError') {
+        throw new UnauthorizedException('Token expired');
+      }
+      if (info?.message) {
+        throw new UnauthorizedException(info.message);
+      }
+      throw new UnauthorizedException('Invalid token');
+    }
+    return user;
+  }
+}
